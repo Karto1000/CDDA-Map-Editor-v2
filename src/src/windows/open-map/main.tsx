8860f16d5f7e1986864b8dfc1ce762875b8728eb
Karto1000 <tim.leuenberger@bluewin.ch>
2025-05-13 09:02:09 +0200

    Add selection of slots when opening map viewer

--- src/src/windows/open-map/main.tsx
+++ src/src/windows/open-map/main.tsx
@@ -2,10 +2,12 @@ import React, {useState} from "react";
 import GenericWindow from "../generic-window.js";
 import MultiMenu from "../../components/multimenu.js";
 import {open} from "@tauri-apps/plugin-dialog";
+import "./main.scss"
 
 function MapViewer() {
     const [mapFilePath, setMapFilePath] = useState<string>("")
-    const [overmapTerrainId, setOvermapTerrainId] = useState<string>("")
+    const [rows, setRows] = useState<number>(1)
+    const [columns, setColumns] = useState<number>(1)
 
     function onSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
@@ -30,14 +32,14 @@ function MapViewer() {
     }
 
     return (
-        <div>
+        <div className={"map-viewer-body"}>
             <p>
                 A map viewer is ideal if you don't want to actually use the map editor to create a map,
                 but still want to see what the map looks like without having to open the main game.
                 The map will be automatically reloaded once it detects a change to the map file which is
                 currently open.
             </p>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit} className={"map-viewer-form"}>
                 <div className={"form-element"}>
                     <label className={"file-input"}>
                         {mapFilePath ? mapFilePath : "Select a Map File Path"}
@@ -47,14 +49,22 @@ function MapViewer() {
                         The path to the map file
                     </label>
                 </div>
-                <div className={"form-element"}>
-                    <input
-                        type={"text"}
-                        name={"overmap-terrain-id"}
-                        placeholder={"Overmap terrain Id"}
-                        value={overmapTerrainId} onChange={(e) => setOvermapTerrainId(e.target.value)}
-                    />
-                    <label htmlFor={"overmap-terrain-id"}>The overmap terrain id</label>
+                <div className={"om-terrain-form-element"}>
+                    {
+                     Array(rows).keys().map(() => {
+                         return (
+                             <div className={"om-terrain-row"}>
+                                 {
+                                     Array(columns).keys().map(() => {
+                                         return (
+                                             <div className={"om-terrain-slot"}/>
+                                         )
+                                     })
+                                 }
+                             </div>
+                         )
+                     })
+                    }
                 </div>
 
                 <button type={"submit"}>Open</button>
