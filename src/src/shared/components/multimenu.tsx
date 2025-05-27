2b2c60083648accfde924233a68be066dbf81e3d
Karto1000 <tim.leuenberger@bluewin.ch>
2025-05-27 16:00:12 +0200

    Add shortcut to navigate through multimenu

--- src/src/shared/components/multimenu.tsx
+++ src/src/shared/components/multimenu.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import "./multimenu.scss"
 import {clsx} from "clsx";
 
@@ -15,6 +15,39 @@ export type MultiMenuProps = {
 
 export default function MultiMenu(props: MultiMenuProps) {
     const [selectedTab, setSelectedTab] = useState<number>(props.tabs.findIndex(t => !t.isDisabled))
+
+    useEffect(() => {
+        const keydownListener = (e: KeyboardEvent) => {
+            if (e.key === "Tab") {
+                e.preventDefault()
+
+                if (e.shiftKey) {
+                    console.log(props.tabs.findLastIndex(t => !t.isDisabled))
+
+                    if (selectedTab === 0) {
+                        setSelectedTab(props.tabs.findLastIndex(t => !t.isDisabled))
+                        return
+                    }
+
+                    setSelectedTab(props.tabs.findLastIndex((t, ti) => !t.isDisabled && ti < selectedTab))
+
+                } else {
+                    if (selectedTab === props.tabs.length - 1) {
+                        setSelectedTab(props.tabs.findIndex(t => !t.isDisabled))
+                        return
+                    }
+
+                    setSelectedTab(props.tabs.findIndex((t, ti) => !t.isDisabled && ti > selectedTab))
+                }
+            }
+        }
+
+        window.addEventListener("keydown", keydownListener)
+
+        return () => {
+            window.removeEventListener("keydown", keydownListener)
+        }
+    }, [selectedTab]);
 
     return (
         <div className={"multimenu"}>
